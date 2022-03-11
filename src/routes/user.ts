d68a137ad6608764ff760818303d1459d1a7d9e6
Jessy Avalos Barrales <jessyavalos16@outlook.es>
2022-03-10 19:16:46 -0600

    signup and signin completed

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,15 +3,17 @@ import User from '../models/User';
 
 const router = Router();
 
-router.post('/signin', (req, res) => {
-    console.log(req.body);
-    res.status(200).send({ response: 'Login successful' })
+router.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+    const data = await User.findOne({ email: email, password: password }, { password: 0, __v: 0 });
+    const response = data ? 'Login successful' : 'Login failed, credentials are invalid.';
+    res.status(200).send({ response, data });
 });
 
 router.post('/signup', async (req, res) => {
-    const newUser = await User.create(req.body);
-    console.log(newUser);
-    res.status(200).send({ response: 'Signup successful' })
+    const data = await User.create(req.body);
+    const response = data ? 'Signup successful' : 'Signup failed, try again.';
+    res.status(200).send({ response, data });
 });
 
 export default router;
