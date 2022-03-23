667b8e30c580a085ff036d06542abca474c4737d
Jessy Avalos Barrales <jessyavalos16@outlook.es>
2022-03-22 20:49:45 -0600

    Heroku

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import Pet from './models/Pet';
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: process.env.URL_CLIENT,
         methods: ["GET", "POST"]
     }
 });
